cfd5e0abf9b563a97f52cf5acec2afb5a9ab3ac9
Salem-2022 <eyerusalem.birru2020@gmail.com>
2022-07-17 19:50:46 +0200

    updated margin

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App mt-4 container-fluid">
-      <header className="App-header text-center">
+      <header className="App-header mb-5 text-center">
        <h1 className="mb-4">Dictionary App</h1>
         <Dictionary />
         
